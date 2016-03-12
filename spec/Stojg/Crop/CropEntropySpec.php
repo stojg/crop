60c349570cd7f11536d9cca965a3406d495a5ffe
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-03-12 18:01:53 +1300

    adding getGrayScaleEntropy()

--- spec/Stojg/Crop/CropEntropySpec.php
+++ spec/Stojg/Crop/CropEntropySpec.php
@@ -12,13 +12,47 @@ class CropEntropySpec extends ObjectBehavior
         $this->shouldHaveType('Stojg\Crop\CropEntropy');
     }
 
-    function it_can_recieve_imagemagic_object_on_construct() {
+    function it_can_recieve_imagemagic_object_on_construct()
+    {
         $this->beConstructedWith(new \Imagick('spec/Stojg/Crop/fixtures/not-a-image.txt'));
         $this->shouldHaveType('Stojg\Crop\CropEntropy');
 
     }
 
-    function it_can_return_an_imagick_object() {
-        $this->getImagick()->shouldHaveType('Imagick');
+    function it_can_return_an_imagick_object()
+    {
+        $this->getImage()->shouldHaveType('Imagick');
+    }
+
+    function it_can_return_the_same_imagick_object_that_it_was_constructed_with()
+    {
+        $obj = new \Imagick('spec/Stojg/Crop/fixtures/not-a-image.txt');
+        $this->beConstructedWith($obj);
+        $this->getImage()->shouldHaveType('Imagick');
+        $this->getImage()->shouldBeLike($obj);
+    }
+
+    function it_can_calculate_an_image_area()
+    {
+        $obj = new \Imagick('spec/Stojg/Crop/fixtures/cloud-01.jpg');
+        $this->beConstructedWith($obj);
+        $this->area()->shouldBeLike(1000 * 600);
+    }
+
+    function it_can_calculate_a_grayscale_entropy_value()
+    {
+        $obj = new \Imagick('spec/Stojg/Crop/fixtures/cloud-01.jpg');
+        $this->beConstructedWith($obj);
+        $this->getGrayScaleEntropy()->shouldBeFloat();
+        $this->getGrayScaleEntropy()->shouldBeCloseTo(11.521439369834);
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'beCloseTo' => function ($subject, $key) {
+                return (abs(($subject - $key) / $subject) < 0.0000000000001);
+            },
+        ];
     }
 }
--- src/Stojg/Crop/CropEntropy.php
+++ src/Stojg/Crop/CropEntropy.php
@@ -7,7 +7,7 @@ class CropEntropy
     /**
      * @var \Imagick
      */
-    protected $imagick = null;
+    protected $image = null;
 
     /**
      * CropEntropy constructor
@@ -17,17 +17,63 @@ class CropEntropy
     public function __construct(\Imagick $image = null)
     {
         if ($image !== null) {
-            $this->imagick = $image;
+            $this->image = $image;
         } else {
-            $this->imagick = new \Imagick();
+            $this->image = new \Imagick();
         }
     }
 
     /**
      * @return \Imagick
      */
-    public function getImagick()
+    public function getImage()
     {
-        return $this->imagick;
+        return $this->image;
+    }
+
+    /**
+     * Get the area in pixels for this image
+     *
+     * @return int
+     */
+    public function area()
+    {
+        $size = $this->image->getImageGeometry();
+        return $size['height'] * $size['width'];
+    }
+
+    /**
+     * Calculate the entropy for this image.
+     *
+     * A higher value of entropy means more noise / liveliness / color / business
+     *
+     * @return float
+     *
+     * @see http://brainacle.com/calculating-image-entropy-with-python-how-and-why.html
+     * @see http://www.mathworks.com/help/toolbox/images/ref/entropy.html
+     */
+    public function getGrayScaleEntropy()
+    {
+        $histogram = $this->image->getImageHistogram();
+        return $this->getEntropy($histogram, $this->area());
+    }
+
+    /**
+     *
+     * @param  \ImagickPixel[] $histogram
+     * @param  int $area
+     * @return float
+     */
+    protected function getEntropy($histogram, $area)
+    {
+        $value = 0.0;
+        foreach ($histogram as $pixel) {
+            // calculates the percentage of pixels having this color value
+            $p = $pixel->getColorCount() / $area;
+            // A common way of representing entropy in scalar
+            $value += $p * log($p, 2);
+        }
+        // $value is always 0.0 or negative, so transform into positive scalar value
+        return -$value;
     }
 }
